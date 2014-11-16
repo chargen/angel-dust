52af272aa718021926ce71787dd634bec0ab39cf
Andrey Konovalov <adech.fo@gmail.com>
2014-11-16 18:52:34 +0400

    Add adaptive color scaling

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"image/color"
 	"image/draw"
 	"image/png"
+	"sort"
 )
 
 func printUsage() {
@@ -35,6 +36,61 @@ func mapBytes(bytes []byte) [][]int {
 	return byteMap
 }
 
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func transformByteMap(byteMap [][]int) [][]int {
+	max := byteMap[0][0]
+	min := byteMap[0][0]
+	for x := 0; x < 256; x++ {
+		for y := 0; y < 256; y++ {
+			max = Max(max, byteMap[x][y])
+			min = Min(min, byteMap[x][y])
+		}
+	}
+
+	if min == max {
+		panic("min == max")
+	}
+
+	heightMap := make(map[int]int)
+	for x := 0; x < 256; x++ {
+		for y := 0; y < 256; y++ {
+			heightMap[byteMap[x][y]] = 0
+		}
+	}
+
+	var heights []int
+	for key := range heightMap {
+		heights = append(heights, key)
+	}
+	sort.Ints(heights)
+
+	step := 256.0 / float32(len(heights))
+	for i, height := range heights {
+		heightMap[height] = int(step * float32(i))
+	}
+
+	for x := 0; x < 256; x++ {
+		for y := 0; y < 256; y++ {
+			byteMap[x][y] = heightMap[byteMap[x][y]]
+		}
+	}
+
+	return byteMap
+}
+
 func convertByteMap(byteMap [][]int) image.Image {
 	img := image.NewRGBA(image.Rect(0, 0, 256, 256))
 	draw.Draw(img, img.Bounds(),
@@ -42,9 +98,9 @@ func convertByteMap(byteMap [][]int) image.Image {
 
 	for x := 0; x < 256; x++ {
 		for y := 0; y < 256; y++ {
-			if byteMap[x][y] > 0 {
-				img.Set(x, y, color.RGBA{255, 255, 255, 255})
-			}
+			colorValue := uint8(byteMap[x][y])
+			img.Set(x, y, color.RGBA{colorValue,
+				colorValue, colorValue, 255})
 		}
 	}
 
@@ -69,6 +125,7 @@ func main() {
 
 	bytes := readBytes(os.Args[1])
 	byteMap := mapBytes(bytes)
+	byteMap = transformByteMap(byteMap)
 	img := convertByteMap(byteMap)
 	saveImage(img, os.Args[2])
 }
